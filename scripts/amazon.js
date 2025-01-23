6c1bfb7ab2853f9c2997e3ea0202e1d76a7735b9
MarkGitonga <markgitonga29@gmail.com>
2025-01-23 17:56:03 +0300

    make the javascript interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach( (product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart"
+          <button class="js-add-to-cart add-to-cart-button button-primary"
           data-product-id="${product.id}">
             Add to Cart
           </button>
@@ -54,11 +54,13 @@ products.forEach( (product) => {
 });
 
 document.querySelector('.js-products-grid') .innerHTML = productsHTML;
+let cart = [];
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
