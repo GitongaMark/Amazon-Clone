681812a8fa187132bdafd536f547272be21a4180
MarkGitonga <markgitonga29@gmail.com>
2026-04-09 22:41:15 +0300

    fix the checkout crash

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -5,7 +5,7 @@ describe('test suite: addToCart', () => {
     spyOn(localStorage, 'setItem');
     
     spyOn(localStorage, 'getItem')
-    .add.callFake(() => {
+    .and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 1,
@@ -25,7 +25,7 @@ describe('test suite: addToCart', () => {
     spyOn(localStorage, 'setItem');
 
     spyOn(localStorage, 'getItem')
-    .add.callFake(() => {
+    .and.callFake(() => {
       return JSON.stringify([]);
     });
     loadFromStorage();
